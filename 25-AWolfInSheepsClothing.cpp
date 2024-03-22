1d3335ca5f24f840fa09c3c3a60902cde33293c1
Brandon Hillig <brandonhillig@gmail.com>
2024-03-21 22:42:11 -0700

    Update 25-AWolfInSheepsClothing.cpp

--- 25-AWolfInSheepsClothing.cpp
+++ 25-AWolfInSheepsClothing.cpp
@@ -1,5 +1,11 @@
 // 25 - A Wolf In Sheeps Clothing (CodeWars Exercise)
 
+/*
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+*/
 #include <vector>
 #include <string>
 
